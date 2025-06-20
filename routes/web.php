fc087bff1d40e8b0fd10d536a99343db57df1185
Senior <senior_gamal@hotmail.com>
2025-06-20 23:19:06 +0400

    Redirect root route to backup servers

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 use App\Http\Controllers\BackupServerController;
+
+Route::redirect('/', '/backupservers');
 
 Route::resource('backupservers', BackupServerController::class);
