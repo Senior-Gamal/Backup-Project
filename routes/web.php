d3db0b36e4c0f3cc0f6b7316d558ab3271164f8c
Senior <senior_gamal@hotmail.com>
2025-06-21 14:27:22 +0400

    Delete routes directory

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BackupServerController;
-
-Route::redirect('/', '/backupservers');
-
-Route::resource('backupservers', BackupServerController::class);
