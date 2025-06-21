e0ebca31edde5039a4455bed75fdd6f9ae787075
Senior <senior_gamal@hotmail.com>
2025-06-22 01:13:28 +0400

    Add role default to user factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'admin@example.com',
             'password' => bcrypt('password'),
+            'role' => 'admin',
         ]);
     }
 }
